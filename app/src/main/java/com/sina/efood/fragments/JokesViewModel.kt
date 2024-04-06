eef69f41ec744e6030e9132b888c4293ee88dced
sina <sina.tabriziyan@gmail.com>
2024-04-06 20:40:06 +0330

    implement joke fragment

--- app/src/main/java/com/sina/efood/fragments/JokesViewModel.kt
+++ app/src/main/java/com/sina/efood/fragments/JokesViewModel.kt
@@ -1,7 +1,10 @@
 package com.sina.efood.fragments
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class JokesViewModel : ViewModel() {
+@HiltViewModel
+class JokesViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
