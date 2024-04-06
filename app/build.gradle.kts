656769e4818db11c876753fa10765ea5860b6c5f
sina <sina.tabriziyan@gmail.com>
2024-04-06 20:36:45 +0330

    add hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kapt)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.hilt)
 
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,5 @@ plugins {
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-
+    alias(libs.plugins.hilt) apply false
 }
