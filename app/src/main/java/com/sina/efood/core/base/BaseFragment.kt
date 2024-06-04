0ad8a8d6e71536e6081a1b358599666a89fb9545
sina <sina.tabriziyan@gmail.com>
2024-06-04 14:40:06 +0330

    create launch when method

--- app/src/main/java/com/sina/efood/core/base/BaseFragment.kt
+++ app/src/main/java/com/sina/efood/core/base/BaseFragment.kt
@@ -5,21 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.viewbinding.ViewBinding
-import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlin.reflect.KClass
+
 
 abstract class BaseFragment<VB : ViewBinding, VM : ViewModel>(
     private val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB,
-    private val viewModelClass: KClass<VM>
+    private val viewModelClass: KClass<VM>,
 ) : Fragment() {
 
 
     private var _binding: VB? = null
     protected val binding: VB
         get() = _binding!!
+
 
     protected val viewModel: VM by lazy {
         ViewModelProvider(this)[viewModelClass.java]
@@ -31,6 +37,7 @@ abstract class BaseFragment<VB : ViewBinding, VM : ViewModel>(
         savedInstanceState: Bundle?
     ): View? {
         _binding = bindingInflater.invoke(inflater, container, false)
+        viewModel
         return binding.root
     }
 
@@ -38,4 +45,15 @@ abstract class BaseFragment<VB : ViewBinding, VM : ViewModel>(
         super.onDestroyView()
         _binding = null
     }
+
+    fun launchWhen(
+        block: suspend CoroutineScope.() -> Unit,
+        state: Lifecycle.State
+    ) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(state, block)
+        }
+    }
+
+
 }
